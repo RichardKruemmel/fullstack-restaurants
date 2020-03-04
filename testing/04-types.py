d4c91751307053f53c1623283eb0d53375ef27e9
Adam Roe <adam.j.roe@gmail.com>
2020-03-04 17:26:51 +0100

    add basic testing examples

---
+++ testing/04-types.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+""" 
+The second file in a series to learn about how testing works.
+
+In this example, we will check if the correct types are being passed into a function
+"""
+
+
+def multiply_two_numbers(x, y):
+    return x*y    
+
+print(multiply_two_numbers(2, 3))
+print(multiply_two_numbers(3,"ha"))
