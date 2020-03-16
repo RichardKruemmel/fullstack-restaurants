3c5e74f4eb26bed0349631ad64d29b55fe0987df
Adam Roe <adam.j.roe@gmail.com>
2020-03-16 11:08:24 +0100

    create static files and serve

--- fullstack/api-ajax-example/backend.py
+++ fullstack/api-ajax-example/backend.py
@@ -1,14 +1,12 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, send_file
 app = Flask(__name__)
 
 
 # define what to do when the user navigates to "/"
-# this could also be served by nginx as a static index.html file.
+# this serves a static html file. 
 @app.route('/')
 def index():
-    content = '<h2>this is a magical full-stack website</h2>'
-    return content
-
+    return send_file("static/html/index.html")
 
 # our mock data - like what we could get from a database
 restaurants = [
@@ -22,10 +20,12 @@ restaurants = [
     }
 ]
 
+# A HTTP RESTful API Route returning the full restaurants dictionary
 @app.route('/api/restaurants/all',  methods=['GET'])
 def api_restaurants_all():
     return jsonify(restaurants)
 
+# A HTTP RESTful API Route returning a list of names of restaurants
 @app.route('/api/restaurants/names',  methods=['GET'])
 def api_restaurants_names():
     # create a new list
