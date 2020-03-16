5955531366015723db687ef23d32cc9f5f463de6
Adam Roe <adam.j.roe@gmail.com>
2020-03-16 12:01:08 +0100

    first version of js get

---
+++ fullstack/api-ajax-example/static/js/getData.js
@@ -0,0 +1,17 @@
+function addTitle() {
+    let title = document.createElement('h2');
+    title.textContent = 'Restaurants in Berlin:';
+    document.body.appendChild(title);
+}
+
+function getAllRestaurantNames(){
+
+    let xhr = new XMLHttpRequest()
+
+    xhr.onload = function () {
+        console.log(this.responseXML)
+    }
+    xhr.open("GET", "http://localhost:5000/api/restaurants/names")
+    xhr.send()
+
+}
