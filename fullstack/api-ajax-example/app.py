e0a446ccfb812142452f3b5706bbca5c197c8865
Richard Krümmel <richardkruemmel@Richards-MacBook-Pro-2.local>
2020-03-19 10:58:59 +0100

    my solution to the weekly challenge

--- fullstack/api-ajax-example/app.py
+++ fullstack/api-ajax-example/app.py
@@ -1,9 +1,16 @@
 from flask import Flask, jsonify, send_file
 from flask_cors import CORS 
+import sqlite3
 
 app = Flask(__name__)
 # enable the api to be accessed by frontend running on localhost
 CORS(app, resources={r"/api/*": {"origins": "http://127.0.0.1"}})
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
 
 # define what to do when the user navigates to "/"
 # this serves a static html file. 
@@ -11,21 +18,16 @@ CORS(app, resources={r"/api/*": {"origins": "http://127.0.0.1"}})
 def index():
     return send_file("static/html/index.html")
 
-# our mock data - like what we could get from a database
-restaurants = [
-    {'id': 0,
-    'name':'Sotto',
-    'neighobrhood':'Wedding'
-    }, 
-    {'id': 1,
-    'name':'Curry 36',
-    'neighobrhood':'Kreuzberg'
-    }
-]
 
 # A HTTP RESTful API Route returning the full restaurants dictionary
 @app.route('/api/restaurants/all',  methods=['GET'])
 def api_restaurants_all():
+    conn = sqlite3.connect('static/restaurants.db')
+    conn.row_factory = dict_factory
+    cur = conn.cursor()
+    restaurants = cur.execute('SELECT * FROM restaurants;').fetchall()
+
+
     return jsonify(restaurants)
 
 # A HTTP RESTful API Route returning a list of names of restaurants
@@ -34,9 +36,13 @@ def api_restaurants_all():
 def api_restaurants_names():
     # create a new list
     restaurant_names = []
+    conn = sqlite3.connect('static/restaurants.db')
+    conn.row_factory = dict_factory
+    cur = conn.cursor()
+    restaurants = cur.execute('SELECT * FROM restaurants;').fetchall()
     # loop through the data, adding the names of each restaurant to our new list 
     for restaurant in restaurants: 
-        restaurant_names.append(restaurant['name'])
+        restaurant_names.append(restaurant['NAME'])
     return jsonify(restaurant_names)
 
 
